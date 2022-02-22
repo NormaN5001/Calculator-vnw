79340c26854f351f7343a7d069366cf77ff8b02d
Nícolas Goulart <nicolas.goulartsouza123@gmail.com>
2022-02-22 20:48:57 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    n1: "",
+    n2: "",
+    res: ""
+  };
+  divide = () =>{
+    let{n1, n2} = this.state
+    if(n1 && n2 !== null){
+      this.setState({
+        res: n1 / n2,
+      })
+    }
+  }
+  multiplic = () =>{
+    let{n1, n2} = this.state
+    if (n1 && n2 !== null){
+      this.setState({
+        res: n1 * n2,
+      })
+    }
+  }
+  subtraction = () =>{
+    let{n1, n2} = this.state
+    if (n1 && n2 !== null){
+      this.setState({
+        res: n1 - n2,
+      })
+    }
+  }
+  addition = () =>{
+    let{n1, n2} = this.state
+    if (n1 && n2 !== null){
+      this.setState({
+        res: n1 + n2,
+      })
+    }
+  }
+  clear = () =>{
+    let{n1, n2} = this.state
+    if (n1 && n2 !== ""){
+      this.setState({
+        n1: 0,
+        n2: 0,
+      })
+    }
+  }
+  handleChange2 = (event) => {
+    this.setState({
+      n2: Number(event.target.value)
+    });
+  };
+
+  handleChange1 = (event) => {
+    this.setState({
+      n1: Number(event.target.value)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Calc App</h1>
+        <input onChange={this.handleChange1} value={this.n1} />
+        <input onChange={this.handleChange2} value={this.n2}/>
+        <button onClick={this.divide}>/</button>
+        <button onClick={this.multiplic}>*</button>
+        <button onClick={this.subtraction}>-</button>
+        <button onClick={this.addition}>+</button>
+        <button onClick={this.clear}>Limpar</button>
+        <h2>O resultado da operação é: {this.state.res}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
